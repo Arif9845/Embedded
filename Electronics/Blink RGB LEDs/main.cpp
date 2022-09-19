21d14a0616e73730e272340c0e6426fedcda24db
Arif Jhalegar <87481864+Arif9845@users.noreply.github.com>
2022-09-19 11:35:35 +0530

    Add files via upload

--- Electronics/Blink RGB LEDs/main.cpp
+++ Electronics/Blink RGB LEDs/main.cpp
@@ -1,1 +1,43 @@
+#include <Arduino.h>
+#define RedLed 4
+#define GreenLed 2
+#define BlueLed 13
 
+void setup()
+{
+  pinMode(RedLed, OUTPUT);
+  pinMode(GreenLed, OUTPUT);
+  pinMode(BlueLed, OUTPUT);
+
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+
+  digitalWrite(RedLed, HIGH);
+  delay(500);
+  digitalWrite(RedLed, LOW);
+  delay(500);
+
+  digitalWrite(GreenLed, HIGH);
+  delay(500);
+  digitalWrite(GreenLed, LOW);
+  delay(500);
+
+  digitalWrite(BlueLed, HIGH);
+  delay(500);
+  digitalWrite(BlueLed, LOW);
+  delay(500);
+
+  digitalWrite(BlueLed, HIGH);
+  digitalWrite(RedLed, HIGH);
+  digitalWrite(GreenLed, HIGH);
+  delay(500);
+  digitalWrite(BlueLed, LOW);
+  digitalWrite(RedLed, LOW);
+  digitalWrite(GreenLed, LOW);
+  delay(500);
+
+  // put your main code here, to run repeatedly:
+}
